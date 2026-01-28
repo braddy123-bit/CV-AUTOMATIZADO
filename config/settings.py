e0cc9ea70cc749d2cee45799538c18bed93e9740
braddy123-bit <braddylondre123@gmail.com>
2026-01-27 21:25:19 -0500

    Fix media uploads using local MEDIA_ROOT

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'imagekit',
+    
     'phonenumber_field',
     'crispy_forms',
     'crispy_bootstrap5',
